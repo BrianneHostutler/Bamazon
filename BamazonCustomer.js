0946f9922a52ba66ef1c9a40784a1ff75ad5b268
Brianne Hostutler <briannehostutler@gmail.com>
2016-04-23 13:41:32 -0400

    prompt improvement

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -20,23 +20,36 @@ connection.connect(function(err){
 		// 		  console.log(res);
 		// });
 
-	prompt.get(["What is the id of the item you would like?", "How many?"], function (err, result) {
+	prompt.get(["id", "howMany"], function (err, result) {
 	    if (err){
 	        console.log(err)
 	    }
 
-		var post  = {id: result.id, howMany: result.howMany};
-		console.log(post);
-		var query = connection.query('INSERT INTO Products SET ?', post, function(err, result) { 
-		});
+		var CustomerPickID = parseInt(result.id);
+		var CustomerQuantity = parseInt(result.howMany);
 
-// if (stockQuantity<=0){
-// 	console.log(Insufficient quantity);
-// }
-// else{
-// UPDATE Products SET stockQuantity = stockQuantity-result.howMany WHERE id = result.id
-// };
+		console.log("id=" + CustomerPickID, "how many=" + CustomerQuantity);
 
+		function selectID(){
+			connection.query('SELECT * FROM Products WHERE ItemID =' + CustomerPickID, function(err, res) { 
+				if (err) throw err;
+				console.log(res);
+			});
+		};
+
+		selectID();	
 	});
+		// var query = connection.query('INSERT INTO Products SET ?', post, function(err, result) { 
+		// });
+
+	
+
+		// if (stockQuantity<=0){
+		// 	console.log("Insufficient quantity");
+		// }
+		// else{
+		// UPDATE Products SET stockQuantity = x WHERE id = x
+		// };
+
 });
 

23e3021b9d8fd274bf3d03591c6ee61379c6c365
Brianne Hostutler <briannehostutler@gmail.com>
2016-04-26 21:04:04 -0400

    added total

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -32,11 +32,14 @@ prompt.get(["id", "howMany"], function (err, result) {
 		connection.query('SELECT * FROM Products WHERE ItemID =' + CustomerPickID, function(err, res) { 
 				if (err) throw err;
 				console.log(res);
-				want = CustomerQuantity;
-       			have = res[0].StockQuantity;
+				var want = CustomerQuantity;
+       			var have = res[0].StockQuantity;
+       			var individPrice = res[0].Price;
        			newQuantity = have - want;
        			if (newQuantity >= 0){
 	          		console.log('Ok! We have enough '+res[0].ProductName+' in stock.');
+	          		var totalCost= individPrice * CustomerQuantity;
+	          		console.log("You owe $" +totalCost);
 	          	}	
        			else if(want>have && have !=0){
 				    console.log('Insufficient quantity. We only have '+have+' in stock.');
